c5e4219db5a8d4d7fe8a2c1dffe3dc7172cc27af
Otavio Voiski <otvoiski@live.com>
2019-08-26 23:51:35 -0300

    ready to use

--- src/config/database.js
+++ src/config/database.js
@@ -3,9 +3,16 @@ const knex = require('knex')(knexfile)
 
 module.exports = knex
 
-knex('books').insert({
-    title: 'Guinness World Records',
-    author: 'Harper Collins',
-    publisher: 'Harper Collins Publishers',
-    releaseDate: new Date(2018,01,01),
-}).then(data => console.log(data))
+// knex('books').insert({
+//     title: 'Guinness World Records',
+//     author: 'Harper Collins',
+//     publisher: 'Harper Collins Publishers',
+//     releaseDate: new Date(2018,01,01),
+// }).then(data => console.log(data))
+
+// knex('books').insert({
+//     title: 'Why We Buy: The Science of Shopping',
+//     author: 'Paco Underhill',
+//     publisher: 'Simon & Schuster',
+//     releaseDate: new Date(2008,12,29),
+// }).then(data => console.log(data))
